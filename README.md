e46e5e10368a4f345767e244dc89c380108b20fd
jhxyjhxy <jhxy2580@gmail.com>
2023-05-28 10:42:39 -0700

    expose but with errors and some unit tests

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+
+Chang Shu, Jennifer Yu
+
+1. Automated tests can be run using a Github action whenever code is pushed. This way, we will be able to know any changes we need to make right when the code is pushed.
+2. No, checking if a singular function is returning the correct output should be a unit test instead of E2E testing. 
+3. x
+4. Yes, because this feature probably won't affect other features. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,23 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// TODO: 2 tests that should be true, 2 tests that should be false FOR EACH FUNCTION
+
+// Testing `isPhoneNumber`
+test('checks for valid phone number with dashes', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('checks for valid phone number with parentheses', () => {
+    expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('checks for invalid phone number (no dashes or parentheses)', () => {
+    expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('checks for invalid phone number (too short)', () => {
+    expect(functions.isPhoneNumber('123-456')).toBe(false);
+});
+
+// Testing `isEmail`
